05735f8b2f11da755a2d07836fe87f4c74da6407
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-07-19 20:07:27 +0700

    up

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/clipanfinance/Desktop/website/exomac-corporate-business-vue-nuxt-js-template-2023-02-25-06-08-12-utc/exomac-test/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
