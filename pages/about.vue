8f55d8f40a8cac6a49b537376364adf4befdf04b
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-07-19 20:11:12 +0700

    aa

--- pages/about.vue
+++ pages/about.vue
@@ -6,6 +6,8 @@
         <BreadcrumbOne activePageName="About Us" title="We are an agency located in New York" backgroundUrl= "/images/bg/breadcrumb-bg.jpg" />
         <AboutTwo class="section-padding-top about-section-padding-bottom-200" />
         <CallToActionVideo />
+        <p>sadsadsadsad</p>
+        <h1>sdadsadsadsa h1</h1>
         <AboutFour />
         <TestimonialSectionOne :style="{backgroundColor: '#f8faff'}" />
         <CallToActionThree />
